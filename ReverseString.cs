c29210f5a017050174cd769cbafb258d4dc07a3f
Pancho545 <58700158+Pancho545@users.noreply.github.com>
2020-01-18 23:10:05 +0200

    Add files via upload

---
+++ ReverseString.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ReverseString
+{
+    class ReverseString
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<char> stackChars = new Stack<char>(input);
+            while (stackChars.Count > 0)
+            {
+                Console.Write(stackChars.Pop());
+            }
+            Console.WriteLine();
+        }
+    }
+}
